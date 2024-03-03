c5d03145a4c49921a1ee57bcb51ed0dccd6d704f
yorumusb <yorumusb@gmail.com>
2024-03-03 16:08:52 +0300

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,8 @@ export default async function Home({ searchParams }) {
             genre === "fetchTopRated"
                 ? `/movie/top_rated`
                 : `/trending/all/week`
-        }?api_key=${API_KEY}&language=en-US&page=1`
+        }?api_key=${API_KEY}&language=en-US&page=1`,
+        { next: { revalidate: 10000 } }
     );
     const data = await res.json();
     if (!res.ok) {
